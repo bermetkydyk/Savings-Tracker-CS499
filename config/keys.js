c36cf550dbb17b3b50bc616db0764b99dd085f34
Bermet Kydykova <38230942+bermetkydyk@users.noreply.github.com>
2020-10-08 18:48:09 -0400

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// ad this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:'996036623698-crmgdiiu4b7rlarf0bi6a17umtutd13v.apps.googleusercontent.com',
+    clientSecret:'3mbX2LeFs1491_MTmPVWTqPs'
+  }
+};
