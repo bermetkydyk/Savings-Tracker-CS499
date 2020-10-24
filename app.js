7a9153421e5c1fdf8ce7ce15fffb5a5057e0330e
JiancongLu <rogerfrontend@gmail.com>
2020-10-24 11:38:25 -0400

    Update App.js

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const key = require('./config/keys');
 
 const app = express();
 
-// set view engine
+app.use(bodyParser.json());
+
+// Enable Cookies
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [key.cookieKey.key]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Database Setup
+//Database 
+const db = require('./config/database');
+const keys = require('./config/keys');
+
+//test the database
+db.authenticate()
+.then(() => console.log('Database connected!'))
+.catch(err => console.log('Error: ' + err))
+
+//user routes
+app.use('/users', require('./routes/userRoutes'));
+
+
+// Routes Setup
 app.set('view engine', 'ejs');
 
 // set up routes
 app.use('/auth', authRoutes);
 
 // create home route
-app.get('/', (req, res) => {
-    res.render('home');
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('app now listening for requests on port 3000');
-});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started OK on port ${PORT}`));
