e07177abd8a2a4984681b0b217d9386bc4d14913
JiancongLu <rogerfrontend@gmail.com>
2020-12-15 18:25:49 -0500

    Add files via upload

--- client/src/components/AddIncome/IncomeNew.js
+++ client/src/components/AddIncome/IncomeNew.js
@@ -2,15 +2,33 @@
 
 import React, { Component } from 'react';
 import IncomeForm from './IncomeForm';
+import IncomeFormReview from './IncomeFormReview';
+import { reduxForm } from 'redux-form';
 
 class IncomeNew extends Component {
+    state = { showFormReview: false };
+
+    renderContent() {
+        if(this.state.showFormReview === true){
+            return <IncomeFormReview 
+                onCancel={ ()=> this.setState({ showFormReview: false })}
+            />;   
+        }
+        
+        return <IncomeForm 
+            onIncomeSubmit={ () => this.setState({ showFormReview: true})
+        }/>;
+    }
+
     render() {
         return (
             <div>
-                <IncomeForm />
+                { this.renderContent() }
             </div>
         );
     }
 }
 
-export default IncomeNew
+export default reduxForm({
+    form: 'incomeForm'
+})(IncomeNew);
