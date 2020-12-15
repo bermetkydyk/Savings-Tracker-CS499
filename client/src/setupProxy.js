518b1602a2c40dfaef0b0eb623409ffe2e2ac4b6
JiancongLu <rogerfrontend@gmail.com>
2020-12-15 18:18:49 -0500

    Add files via upload

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -21,4 +21,75 @@ module.exports = function(app) {
           changeOrigin: true,
         })
     );
+    app.use(
+      '/userExpenses/add/',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userExpenses/*',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userExpenses/remove/*',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userIncomes/*',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userIncomes/remove/*',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userIncomes/add/',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userIncomes/currentUser/add',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userIncomes/currentUser',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/userGoals/*',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+    app.use(
+      '/api/file/upload',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+    );
+
 };
