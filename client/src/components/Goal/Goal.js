e4a42a2a1aad2d001efeccbd81db44545fc56f62
JiancongLu <rogerfrontend@gmail.com>
2020-12-15 18:22:59 -0500

    Add files via upload

--- client/src/components/Goal/Goal.js
+++ client/src/components/Goal/Goal.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchExpenses, fetchIncomes, fetchGoals } from '../../actions';
+
 import M from "materialize-css";
-
-
 
 class Goal extends Component {
   
     componentDidMount() {
-        let carousel = document.querySelectorAll('.carousel.carousel-slider');
-    
-        let options = {
-            fullWidth: true,
-            indicators: true
-        };
-        
-        M.Carousel.init(carousel, options);
+        // Initializing MaterializeCSS  javascript component 
+        //let tabs = document.querySelectorAll('.tabs');
+        //let options = {
+      
+        //};  
+        //M.Tabs.init(tabs, options);
+
+        // Fetching data from Database with Aios
+        this.props.fetchExpenses();
+        this.props.fetchIncomes();
+        this.props.fetchGoals();
     }
-  
+    
+    renderGoals(){
+        //console.log(this.props);
+        return this.props.goals.map(goal => {
+            return (
+                <div className="container">
+                    <div className='card darken-1' key={goal.goalId}>
+                        <div className="card-content">
+                            <span className="card-title">Goal: {goal.title}</span>
+                            <p>Amount Needed: {goal.amountNeeded}</p>
+                            <p>Description: {goal.description}</p>
+                            <p>Reach by Date: {goal.reachByDate}</p>
+                            <p>Priority: {goal.priority}</p>
+                            <p>Created At: {goal.createdAt}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
 
         return (
-            <div>
-                <h1>Goal Page</h1>
-            </div>
+            <>
+                <nav>
+                    <div className="nav-wrapper white">
+                        <p className="brand-logo center teal-text" style={{margin: "0px"}}>Goal</p>
+                    </div>
+                </nav>
+                {this.renderGoals()}
+               
+            </>
         );
     }
 }
 
-export default Goal;
+function mapStateToProps(state) {
+    console.log(state);
+    return { expenses: state.expenses, incomes: state.incomes, goals: state.goals };
+}
+
+export default connect(mapStateToProps, { fetchExpenses, fetchIncomes, fetchGoals })(Goal);
