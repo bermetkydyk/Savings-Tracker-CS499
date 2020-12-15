66b9a919186573dd6ee217d1e9b573fe1f8f18e7
JiancongLu <rogerfrontend@gmail.com>
2020-12-15 18:48:45 -0500

    Add files via upload

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_EXPENSES, FETCH_INCOMES, FETCH_GOALS } from './types';
 
 export const fetchUser = () => {
     return function(dispatch) {
@@ -11,3 +11,48 @@ export const fetchUser = () => {
     };
 };
 
+export const submitIncome = (values, history) => async dispatch => {
+    const res = await axios.post('/userIncomes/add/', values);
+    history.push('/summary');
+    //dispatch({ type: FETCH_USER, palyload: res.data });
+};
+
+export const submitExpense = (values, history) => async dispatch => {
+    const res = await axios.post('/userExpenses/add/', values);
+    history.push('/summary');
+};
+
+export const submitGoal = (values, history) => async dispatch => {
+    const res = await axios.post('/userGoals/add/', values);
+    history.push('/goal');
+};
+
+export const fetchExpenses = () => {
+    return function(dispatch) {
+        axios.get('/userExpenses/currentUser')
+            .then(res => dispatch({
+                type: FETCH_EXPENSES,
+                payload: res.data
+            }));
+    };
+};
+
+export const fetchIncomes = () => {
+    return function(dispatch) {
+        axios.get('/userIncomes/currentUser')
+            .then(res => dispatch({
+                type: FETCH_INCOMES,
+                payload: res.data
+            }));
+    };
+};
+
+export const fetchGoals = () => {
+    return function(dispatch) {
+        axios.get('/userGoals/currentUser')
+            .then(res => dispatch({
+                type: FETCH_GOALS,
+                payload: res.data
+            }));
+    };
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,4 @@
 export const FETCH_USER = 'fetch_user';
+export const FETCH_EXPENSES = 'fetch_expenses';
+export const FETCH_INCOMES = 'fetch_incomes';
+export const FETCH_GOALS = 'fetch_goals';
