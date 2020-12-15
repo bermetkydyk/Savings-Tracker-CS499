8c4542cfd454441bfc36ca934bddcf48b1a81fc7
JiancongLu <rogerfrontend@gmail.com>
2020-12-15 18:25:26 -0500

    Add files via upload

--- client/src/components/AddExpense/ExpenseNew.js
+++ client/src/components/AddExpense/ExpenseNew.js
@@ -1,16 +1,34 @@
 // ExpenseNew shows ExpenseForm and ExpenseReview
-
 import React, { Component } from 'react';
 import ExpenseForm from './ExpenseForm';
+import ExpenseFormReview from './ExpenseFormReview';
+import { reduxForm } from 'redux-form';
 
 class ExpenseNew extends Component {
+    state = { showFormReview: false };
+
+    renderContent() {
+        if(this.state.showFormReview === true){
+            return <ExpenseFormReview 
+                onCancel={ ()=> this.setState({ showFormReview: false })}
+            />;   
+        }
+        
+        return <ExpenseForm 
+            onExpenseSubmit={ () => this.setState({ showFormReview: true})
+        }/>;
+    }
+
+
     render() {
         return (
             <div>
-                <ExpenseForm />
+                {this.renderContent()}
             </div>
         );
     }
 }
 
-export default ExpenseNew;
+export default reduxForm({
+    form: 'expenseForm'
+})(ExpenseNew);
