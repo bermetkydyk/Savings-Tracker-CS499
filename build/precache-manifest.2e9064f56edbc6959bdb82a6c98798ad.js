6e2f7222b57d5789e9766c698cccabd80e6cbc19
JiancongLu <rogerfrontend@gmail.com>
2020-12-18 06:06:02 -0500

    Build the frontend with Webpack

---
+++ build/precache-manifest.2e9064f56edbc6959bdb82a6c98798ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42996a0b330d79e4b339adc3411f602a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9203c01f6d41c736f4f3",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "08feef9f16bcbae528a7",
+    "url": "/static/css/main.05b3771a.chunk.css"
+  },
+  {
+    "revision": "9203c01f6d41c736f4f3",
+    "url": "/static/js/2.da7145a9.chunk.js"
+  },
+  {
+    "revision": "15c50d37eb12b8b1e72cf492c12ebcf5",
+    "url": "/static/js/2.da7145a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08feef9f16bcbae528a7",
+    "url": "/static/js/main.100444dd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5c0722c4695b100bff9a9fa570469336",
+    "url": "/static/media/Bottombar.5c0722c4.png"
+  },
+  {
+    "revision": "a34eae130c2b7288be79a03c23db5d84",
+    "url": "/static/media/guide_pic.a34eae13.png"
+  }
+]);
